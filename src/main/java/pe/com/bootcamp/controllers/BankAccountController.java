8241b1b467c5dc01c2093ae9a36c01e34d3db43f
Luis Eduardo Cochachi Chamorro <109692666+cochachyLE-Dev@users.noreply.github.com>
2022-09-08 19:03:07 -0500

    .

--- src/main/java/pe/com/bootcamp/controllers/BankAccountController.java
+++ src/main/java/pe/com/bootcamp/controllers/BankAccountController.java
@@ -2,6 +2,9 @@ package pe.com.bootcamp.controllers;
 
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Import;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,7 +16,9 @@ import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import pe.com.bootcamp.configurations.WebClientInstance1Config;
 import pe.com.bootcamp.domain.aggregate.BankAccount;
 import pe.com.bootcamp.domain.fallback.BankAccountFallback;
+import pe.com.bootcamp.utilities.ResultBase;
 import pe.com.bootcamp.utilities.UnitResult;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @RestController
@@ -27,16 +32,77 @@ public class BankAccountController extends BankAccountFallback {
 		webClient = loadBalancerBankAccount.build();
 	}
 	
-	@RequestMapping(value = "/", method = RequestMethod.GET)	
-	@HystrixCommand(fallbackMethod = "fallback_findAll", commandProperties = {
-			   @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "1000")
-			})
-	public Mono<UnitResult<BankAccount>> findAll() throws InterruptedException{		
+	@RequestMapping(value = "/", method = RequestMethod.POST)
+	@HystrixCommand(
+			fallbackMethod = "fallback_create",
+			commandProperties = {@HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "15000")})
+	public Mono<UnitResult<BankAccount>> create(@RequestBody BankAccount entity){
+		return webClient.post().uri("/")
+				.bodyValue(entity)
+				.retrieve().bodyToMono(new UnitResult<BankAccount>().getClassOfT());
+	}
+	@RequestMapping(value = "/", method = RequestMethod.PUT)	
+	@HystrixCommand(
+			fallbackMethod = "fallback_update",
+			commandProperties = {@HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "15000")})
+	public Mono<UnitResult<BankAccount>> update(@RequestBody BankAccount entity){
+		return webClient.put().uri("/")
+				.bodyValue(entity)
+				.retrieve().bodyToMono(new UnitResult<BankAccount>().getClassOfT());
+	}
+	@RequestMapping(value = "/batch", method = RequestMethod.POST)
+	@HystrixCommand(
+			fallbackMethod = "fallback_saveAll",
+			commandProperties = {@HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "5000")})
+	public Mono<UnitResult<BankAccount>> saveAll(@RequestBody Flux<BankAccount> entities){
+		return webClient.post().uri("/batch")
+				.bodyValue(entities)
+				.retrieve().bodyToMono(new UnitResult<BankAccount>().getClassOfT());
+	}
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	@HystrixCommand(
+			fallbackMethod = "fallback_findById",
+			commandProperties = {@HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "5000")})
+	public Mono<UnitResult<BankAccount>> findById(@PathVariable String id){
+		return webClient.get().uri("/{id}", id)
+				.retrieve().bodyToMono(new UnitResult<BankAccount>().getClassOfT());
+	}
+	@RequestMapping(value = "/{dni}", method = RequestMethod.GET)
+	@HystrixCommand(
+			fallbackMethod = "fallback_findByClientIdentNum",
+			commandProperties = {@HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "5000")})
+	public Mono<UnitResult<BankAccount>> findByClientIdentNum(@PathVariable String dni){
+		return webClient.get().uri("/{dni}", dni)
+				.retrieve().bodyToMono(new UnitResult<BankAccount>().getClassOfT());
+	}
+	@RequestMapping(value = "/{accountNumber}", method = RequestMethod.POST)
+	@HystrixCommand(
+			fallbackMethod = "fallback_findByAccountNumber",
+			commandProperties = {@HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "5000")})
+	public Mono<UnitResult<BankAccount>> findByAccountNumber(@PathVariable String accountNumber){
+		return webClient.get().uri("/{accountNumber}", accountNumber)
+				.retrieve().bodyToMono(new UnitResult<BankAccount>().getClassOfT());
+	}
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	@HystrixCommand(
+			fallbackMethod = "fallback_findAll",
+			commandProperties = {@HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "5000")})
+	public Mono<UnitResult<BankAccount>> findAll(){
 		//Thread.sleep(3000);
-		
-		Mono<UnitResult<BankAccount>> result = webClient.get().uri("/")
-		.retrieve().bodyToMono(new UnitResult<BankAccount>().getClassOfT());
-		
-		return result;
+		return webClient.get().uri("/").retrieve().bodyToMono(new UnitResult<BankAccount>().getClassOfT());
 	}	
+	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+	@HystrixCommand(
+			fallbackMethod = "fallback_deleteById",
+			commandProperties = {@HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "5000")})
+	public Mono<ResultBase> deleteById(@PathVariable String id){
+		return webClient.delete().uri("/{id}", id).retrieve().bodyToMono(ResultBase.class);
+	}
+	@RequestMapping(value = "/stream", method = RequestMethod.GET,produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+	@HystrixCommand(
+			fallbackMethod = "fallback_findAllStreaming",
+			commandProperties = {@HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "5000")})
+	public Flux<BankAccount> findAllStreaming(){
+		return webClient.get().uri("/stream").retrieve().bodyToFlux(BankAccount.class);
+	}
 }
--- src/main/java/pe/com/bootcamp/domain/fallback/BankAccountFallback.java
+++ src/main/java/pe/com/bootcamp/domain/fallback/BankAccountFallback.java
@@ -1,16 +1,62 @@
 package pe.com.bootcamp.domain.fallback;
 
 import pe.com.bootcamp.domain.aggregate.BankAccount;
+import pe.com.bootcamp.utilities.ResultBase;
 import pe.com.bootcamp.utilities.UnitResult;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 public class BankAccountFallback {
 	
-	Mono<UnitResult<BankAccount>> fallback_findAll() {
+	Mono<UnitResult<BankAccount>> fallback_create(BankAccount entity){
 		int code = 1002;
-		String[] causes = new String[1];
-		causes[0] = "Timeout expired";
-		
+		String[] causes = new String[] { "Timeout expired" };				
+		return Mono.just(new UnitResult<BankAccount>(true,"Request fails. It takes long time to response", code, causes));			
+	}
+	
+	Mono<UnitResult<BankAccount>> fallback_update(BankAccount entity){
+		int code = 1002;
+		String[] causes = new String[] { "Timeout expired" };				
+		return Mono.just(new UnitResult<BankAccount>(true,"Request fails. It takes long time to response", code, causes));			
+	}
+	
+	Mono<UnitResult<BankAccount>> fallback_saveAll(Flux<BankAccount> entities){
+		int code = 1002;
+		String[] causes = new String[] { "Timeout expired" };				
+		return Mono.just(new UnitResult<BankAccount>(true,"Request fails. It takes long time to response", code, causes));			
+	}
+	
+	Mono<UnitResult<BankAccount>> fallback_findById(String id){
+		int code = 1002;
+		String[] causes = new String[] { "Timeout expired" };				
+		return Mono.just(new UnitResult<BankAccount>(true,"Request fails. It takes long time to response", code, causes));			
+	}
+	
+	Mono<UnitResult<BankAccount>> fallback_findByClientIdentNum(String dni){
+		int code = 1002;
+		String[] causes = new String[] { "Timeout expired" };				
+		return Mono.just(new UnitResult<BankAccount>(true,"Request fails. It takes long time to response", code, causes));			
+	}
+	
+	Mono<UnitResult<BankAccount>> fallback_findByAccountNumber(String accountNumber){
+		int code = 1002;
+		String[] causes = new String[] { "Timeout expired" };				
+		return Mono.just(new UnitResult<BankAccount>(true,"Request fails. It takes long time to response", code, causes));			
+	}
+	
+	Mono<UnitResult<BankAccount>> fallback_findAll(){
+		int code = 1002;
+		String[] causes = new String[] { "Timeout expired" };				
+		return Mono.just(new UnitResult<BankAccount>(true,"Request fails. It takes long time to response", code, causes));			
+	}
+	
+	Flux<BankAccount> fallback_findAllStreaming() throws Exception{
+		throw new Exception("Timeout expired");			
+	}
+	
+	Mono<ResultBase> fallback_deleteById(String id){
+		int code = 1002;
+		String[] causes = new String[] { "Timeout expired" };				
 		return Mono.just(new UnitResult<BankAccount>(true,"Request fails. It takes long time to response", code, causes));			
 	}
 }
